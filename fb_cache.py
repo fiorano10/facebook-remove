cf53370502acd3eee9ceb4d79af33f92159d09fb
Vibhor Sood <vibhorsood1992@gmail.com>
2017-11-27 11:48:11 -0500

    Update fb_cache.py

--- fb_cache.py
+++ fb_cache.py
@@ -6,15 +6,15 @@ import sys
 import glob
 import errno
 
-rootdir = '/Users/VibhorSood/Library/Caches/com.apple.Safari/'
+rootdir = '/Users/VibhorSood/Library/Safari/'
 #Removing Facebook cache from all folders under Safari
 
-for subdir, dirs, files3 in os.walk(rootdir):
-    for file in files3:
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
         filepath = os.path.join(subdir,file)
         with open(filepath,'rb') as f:
-            cont3 = f.read()
-        if 'facebook' in cont3:
+            cont = f.read()
+        if 'facebook' in cont:
             os.remove(os.path.join(subdir,file))
             #list all files that contain the keyword
             #print os.path.join(subdir, file)
