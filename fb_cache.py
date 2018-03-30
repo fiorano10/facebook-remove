f76ff9a547a31ec01d27fded8cad39c1265c699a
Vibhor Sood <vibhorsood1992@gmail.com>
2018-03-29 23:07:29 -0400

    'Should' also remove Instagram cache

--- fb_cache.py
+++ fb_cache.py
@@ -1,23 +1,24 @@
+#Author : VibhorSood
+#Date: Nov 2017
 #!/usr/bin/env python
 
 import re
 import os
-import sys
 import glob
-import errno
+import time
 
+#Removing Facebook, and Instagram cache
+
+start_time = time.time()
 rootdir = '/Users/VibhorSood/Library/Safari/'
-<<<<<<< HEAD
-=======
-#Removing Facebook cache from all folders under Safari
-
->>>>>>> cf53370502acd3eee9ceb4d79af33f92159d09fb
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         filepath = os.path.join(subdir,file)
         with open(filepath,'rb') as f:
             cont = f.read()
         if 'facebook' in cont:
+            os.remove(os.path.join(subdir,file))
+        elif 'instagram' in cont:
             os.remove(os.path.join(subdir,file))
             #print os.path.join(subdir, file)
 
@@ -29,3 +30,6 @@ for subdir2, dirs2, files2 in os.walk(newdir):
             cont2 = f2.read()
         if 'facebook' in cont2:
             os.remove(os.path.join(subdir2,file))
+        elif 'instagram' in cont2:
+            os.remove(os.path.join(subdir2,file))
+print "It took", time.time() - start_time, "to run"
